d4a1da96f638fe0942fb34b2cab919626378c92f
Maksim Bobrovskiy <74147249+Slimenion@users.noreply.github.com>
2020-12-30 16:01:44 +0300

    Update httpServer.cpp

--- httpServer/httpServer.cpp
+++ httpServer/httpServer.cpp
@@ -2,8 +2,7 @@
 #include <sstream>
 #include <string>
 
-// Для корректной работы freeaddrinfo в MinGW
-// Подробнее: http://stackoverflow.com/a/20306451
+
 #define _WIN32_WINNT 0x501
 
 #include <WinSock2.h>
